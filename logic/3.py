1538f36acd3cfec4afc2c5d316df81943b9ddb56
GUNAPATISANTHOSH <santoshsantutej@gmail.com>
2026-06-23 16:26:46 +0530

    Update 3.py and add 4.py

--- logic/3.py
+++ logic/3.py
@@ -1,6 +1,6 @@
 # largest number
 nums = [12, 45, 7, 89, 23]
-largest = 0
+largest = nums[0]
 for num in nums:
     if num> largest:
         largest=num
---
+++ logic/4.py
@@ -0,0 +1,7 @@
+# smallest number
+nums = [12, 45, 7, 89, 23]
+smallest = nums[0]
+for num in nums:
+    if num<smallest:
+        smallest=num
+print(smallest)

ca2dc06d0b49e9ace15a6b21de9d71d72809f874
GUNAPATISANTHOSH <santoshsantutej@gmail.com>
2026-07-22 17:01:45 +0530

    oop

---
+++ oop/practice.py
@@ -0,0 +1,13 @@
+# class Car:
+#     name="volvo"
+#     color="red"
+# c1=Car()
+# print(c1.name)
+# print(c1.color)  
+
+class Student:
+    def studentDetails(self):
+        print("hello student")
+s1=Student()
+s1.studentDetails()
+
